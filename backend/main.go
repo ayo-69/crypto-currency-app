0b32eeb470be4190f589f13153f4b213f8e50b29
Isaac Hayab <isaachayab0@gmail.com>
2025-08-30 21:44:28 +0200

    Added CORS

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,14 @@ import (
 	"os"
 
 	"github.com/ayo-69/crypto-price-tracker/backend/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	godotenv.Load()
 	PORT := os.Getenv("PORT")
